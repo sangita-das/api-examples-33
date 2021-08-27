269011f942e549da063dba9920dd4e0f43ed66a7
unknown <mistymoni5050@gmail.comgit config --global user.name sangita-das>
2021-08-27 15:37:33 +0600

    Add files via upload

---
+++ js/buddy.js
@@ -0,0 +1,22 @@
+const loadBuddies = () => {
+  fetch('https://randomuser.me/api/?results=5')
+    .then(res => res.json())
+    .then(data => displayBuddies(data))
+}
+
+loadBuddies();
+
+const displayBuddies = data => {
+  const buddies = data.results;
+  // console.log(data.results);
+  const buddiesDiv = document.getElementById('buddies');
+  for (const buddy of buddies) {
+    console.log(buddy.name.first, buddy.name.last, buddy.email)
+    const p = document.createElement('p');
+    p.innerText = `
+    Name: ${buddy.name.title} ${buddy.name.first} ${buddy.name.last} email:${buddy.email} id: ${buddy.id.value}
+  
+  `;
+    buddiesDiv.appendChild(p);
+  }
+}
---
+++ js/countries.js
@@ -0,0 +1,64 @@
+const loadCountries = () => {
+  fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(data => displayCountries(data))
+}
+
+loadCountries();
+
+
+const displayCountries = countries => {
+  // console.log(countries)
+  /* for (const country of countries) {
+    console.log(country);
+  } */
+
+  // or  same thing happend in upper two line or lower two line
+
+  /*  countries.forEach(country => {
+     console.log(country);
+   }); */
+
+
+  const countriesDiv = document.getElementById('countries')
+  countries.forEach(country => {
+    // console.log(country);
+    const div = document.createElement('div')
+    div.classList.add('country')
+
+    div.innerHTML = `
+<h3>${country.name}</h3>
+<p>${country.capital}</p>
+<button onclick = "loadCountryByName('${country.name}')">Details</button>
+      `;
+
+
+    // const h3 = document.createElement('h3');
+    // h3.innerText = country.name;
+    // div.appendChild(h3);
+    // const p = document.createElement('p');
+    // p.innerText = country.capital;
+    // div.appendChild(p);
+    countriesDiv.appendChild(div)
+  })
+
+}
+
+const loadCountryByName = name => {
+  const url = `https://restcountries.eu/rest/v2/name/${name}`
+  // console.log(url);
+  fetch(url)
+    .then(res => res.json())
+    .then(data => displayCountryDetail(data[0]));
+}
+
+
+const displayCountryDetail = country => {
+  console.log(country);
+  const countryDiv = document.getElementById('country-detail');
+  countryDiv.innerHTML = `
+      <h5>${country.name}</h4>
+      <p>population: ${country.population}</p>
+      <img width="200px" src="${country.flag}">
+  `
+}
